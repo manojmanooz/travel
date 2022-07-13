629226ec2a8a014b74153084cfbeceebd8fd382d
MANOJ T <manojthiyagaraj57@gmail.com>
2022-07-13 16:08:15 +0530

    addedd

---
+++ demoproject/demoapp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import place,team
+# Create your views here.
+def demo(request):
+    obj=place.objects.all()
+    obj1=team.objects.all()
+    return render(request,'index.html',{'result':obj,'result1':obj1})
